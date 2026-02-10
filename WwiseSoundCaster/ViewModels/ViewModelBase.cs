054d23ab3807f436e7bc93bce1ac44c768196a04
JianghaoL <daniel_ljh@126.com>
2026-02-09 18:27:14 -0800

    initial commit

---
+++ WwiseSoundCaster/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WwiseSoundCaster.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ WwiseSoundCaster/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WwiseSoundCaster.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
